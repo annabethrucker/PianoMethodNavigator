5986247ce35c4fe310e5c36380fafaca2c63b178
annabethrucker <annabethrucker@gmail.com>
2025-07-21 12:59:42 -0700

    app.py

--- app.py
+++ app.py
@@ -121,4 +121,6 @@ def index():
     result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
